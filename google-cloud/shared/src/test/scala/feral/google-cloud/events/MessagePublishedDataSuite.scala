03002c0c0103b0c5dcb37035bbbd3cee5bc8ccfe
Yummy-Yums <eliasderby90@gmail.com>
2026-07-03 18:13:06 +0000

    forgot to update messagepublished_data_suite

--- google-cloud/shared/src/test/scala/feral/google-cloud/events/MessagePublishedDataSuite.scala
+++ google-cloud/shared/src/test/scala/feral/google-cloud/events/MessagePublishedDataSuite.scala
@@ -24,25 +24,20 @@ import java.time.Instant
 class MessagePublishedDataSuite extends FunSuite {
 
   test("decoder") {
-    val data = event.as[MessagePublishedData].toTry.get
-    assertEquals(data.subscription, "projects/my-project/subscriptions/my-subscription")
-    assertEquals(data.message.data, "dGVzdCBtZXNzYWdlIDM=")
-    assertEquals(data.message.attributes, Map("attr1" -> "attr1-value"))
-    assertEquals(data.message.messageId, "message-id")
-    assertEquals(data.message.publishTime, Instant.parse("2021-02-05T04:06:14.109Z"))
+    assertEquals(event.as[MessagePublishedData].toTry.get, result)
   }
 
   def event = json"""
     {
-        "subscription": "projects/my-project/subscriptions/my-subscription",
-        "message": {
-            "attributes": {
-                "attr1":"attr1-value"
-            },
-            "data": "dGVzdCBtZXNzYWdlIDM=",
-            "messageId": "message-id",
-            "publishTime":"2021-02-05T04:06:14.109Z"
-        }
+      "subscription": "projects/my-project/subscriptions/my-subscription",
+      "message": {
+        "attributes": {
+            "attr1":"attr1-value"
+        },
+        "data": "dGVzdCBtZXNzYWdlIDM=",
+        "messageId": "message-id",
+        "publishTime":"2021-02-05T04:06:14.109Z"
+      }
     }
   """
 
